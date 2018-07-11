73317b21e6610f7208796dcd7bdc4a41f2fd5b58
Frank Polster <frank.polster@maibornwolff.de>
2018-07-11 10:59:52 +0200

    added prometheustoken to deployment

---
+++ contracts/PrometheusToken.sol
@@ -0,0 +1,18 @@
+pragma solidity 0.4.24;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract PrometheusToken is StandardToken {
+
+    struct Data {
+        uint km;
+    }
+
+    function addData() public {
+
+    }
+
+    function getData() public {
+
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var PrometheusToken = artifacts.require("./PrometheusToken.sol");
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(PrometheusToken);
 };
